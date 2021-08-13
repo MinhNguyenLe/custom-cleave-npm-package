93b316f2252978c598e95a0b4d85b997ff22dadd
ZinZinNguyen <71427292+ZinZinNguyen@users.noreply.github.com>
2021-08-13 12:20:16 +0700

    test publish version for my package

--- src/libs/creditCardFormatter.js
+++ src/libs/creditCardFormatter.js
@@ -1,6 +1,6 @@
 const creditCardFormatter = (options) => {
   // let credit = document.querySelectorAll(options.element);
-  console.log(options.element);
+  console.log(options.element, "oyeeee");
 };
 
 module.exports = creditCardFormatter;
